4b9bf05b4db12cc31256e2d0daed4ac058656a19
SaravanaMK <saravanatweak@gmail.com>
2018-03-14 06:58:16 +0530

    Initial commit

---
+++ src/main/java/com/guitar/CustomRestMvcConfig.java
@@ -0,0 +1,13 @@
+/*package com.guitar;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+*/
+/*@Configuration
+public class CustomRestMvcConfig extends RepositoryRestConfiguration{
+	@Override
+	public RepositoryRestConfiguration config () {
+		RepositoryRestConfiguration config = super.config();
+		config.setBasePath("/api");
+		return config;
+}	}*/
